db9884f103c6ab2bff8f132773b4adc32a97233e
sdboyer <drupal@samboyer.org>
2013-06-30 22:49:01 -0400

    Let the AssetLibraryRepository populate itself.
    This is suuuuper dirty.

--- core/lib/Drupal/Core/Asset/AssetLibraryRepository.php
+++ core/lib/Drupal/Core/Asset/AssetLibraryRepository.php
@@ -2,28 +2,91 @@
 
 /**
  * @file
- * Contains \Drupal\Core\Asset\AssetLibraryManager.
+ * Contains \Drupal\Core\Asset\AssetLibraryRepository.
  */
 
 namespace Drupal\Core\Asset;
 
 use Drupal\Core\Asset\AssetLibrary;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 
-class AssetLibraryManager implements \IteratorAggregate {
+/**
+ * @todo Transform this into a 'lazy' library - serialize & load as needed.
+ */
+class AssetLibraryRepository implements \IteratorAggregate {
+
   protected $libraries;
 
   protected $flattened;
 
   /**
+   * Indicates whether or not the repository has initialized its collection.
+   *
+   * @todo this is very dirty; shift responsibility for populating to something external
+   *
+   * @var bool
+   */
+  protected $initialized = FALSE;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  function __construct($module_handler) {
+    $this->moduleHandler = $module_handler;
+  }
+
+  protected function initialize() {
+    if ($this->initialized) {
+      return;
+    }
+    $this->initialized = TRUE;
+
+    $library_collector = new AssetLibraryCollector($this);
+    foreach ($this->moduleHandler->getImplementations('library_info') as $module) {
+      $library_collector->setModule($module);
+      $libraries = call_user_func("{$module}_library_info");
+      foreach ($libraries as $name => $info) {
+        // Normalize - apparently hook_library_info is allowed to be sloppy.
+        $info += array('dependencies' => array(), 'js' => array(), 'css' => array());
+
+        // @todo This works sorta sanely because of the array_intersect_key() hack in AssetLibrary::construct()
+        $asset_collector = $library_collector->buildLibrary($name, $info);
+        foreach (array('js', 'css') as $type) {
+          if (!empty($info[$type])) {
+            foreach ($info[$type] as $data => $options) {
+              if (is_scalar($options)) {
+                $data = $options;
+                $options = array();
+              }
+              // @todo good enough for now to assume these are all file assets
+              $asset_collector->create($type, 'file', $data, $options);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
    * Gets a library by composite key.
    *
-   * @param string $name The library name
+   * @param string $module
+   *   The module owner that declared the library.
    *
-   * @return AssetLibrary The library
+   * @param string $name
+   *   The library name.
+   *
+   * @return \Drupal\Core\Asset\AssetLibrary
+   *   The requested library.
    *
    * @throws \InvalidArgumentException If there is no library by that name
    */
   public function get($module, $name) {
+    $this->initialize();
     if (!isset($this->libraries[$module][$name])) {
       throw new \InvalidArgumentException(sprintf('There is library identified by "%s/%s" in the manager.', $module, $name));
     }
@@ -34,11 +97,17 @@ class AssetLibraryManager implements \IteratorAggregate {
   /**
    * Checks if the current library manager has a certain library.
    *
-   * @param string $name an library name
+   * @param string $module
+   *   The module owner that declared the library.
    *
-   * @return Boolean True if the library has been set, false if not
+   * @param string $name
+   *   The library name.
+   *
+   * @return bool
+   *   True if the library has been set, false if not
    */
   public function has($module, $name) {
+    $this->initialize();
     return isset($this->libraries[$module][$name]);
   }
 
@@ -58,6 +127,7 @@ class AssetLibraryManager implements \IteratorAggregate {
    * @return array An array of library names
    */
   public function getNames() {
+    $this->initialize();
     return array_keys($this->libraries);
   }
 
@@ -65,11 +135,13 @@ class AssetLibraryManager implements \IteratorAggregate {
    * Clears all libraries.
    */
   public function clear() {
+    $this->initialize();
     $this->libraries = array();
     $this->flattened = NULL;
   }
 
   public function getIterator() {
+    $this->initialize();
     if (is_null($this->flattened)) {
       $this->flattened = array();
       foreach ($this->libraries as $module => $set) {
@@ -81,4 +153,5 @@ class AssetLibraryManager implements \IteratorAggregate {
 
     return new \ArrayIterator($this->flattened);
   }
+
 }
