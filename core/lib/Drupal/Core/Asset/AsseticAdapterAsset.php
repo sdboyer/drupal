3add5d679534308243eedd2d1d06f901c700f8ff
sdboyer <drupal@samboyer.org>
2013-09-22 16:58:00 +0200

    Reuse Assetic's BaseAsset.
    This could create some issues with the private members used there, but
    reduces code duplication enough that it's worth trying for now.

--- core/lib/Drupal/Core/Asset/AsseticAdapterAsset.php
+++ core/lib/Drupal/Core/Asset/AsseticAdapterAsset.php
@@ -8,15 +8,14 @@
 namespace Drupal\Core\Asset;
 
 use Assetic\Asset\AssetInterface;
+use Assetic\Asset\BaseAsset as AsseticBaseAsset;
 use Drupal\Core\Asset\Exception\UnsupportedAsseticBehaviorException;
 
 /**
  * A class that reduces boilerplate code by centrally disabling the Assetic
  * properties and methods Drupal does not support.
- *
- * TODO extend Assetic's BaseAsset
  */
-abstract class AsseticAdapterAsset implements AssetInterface {
+abstract class AsseticAdapterAsset extends AsseticBaseAsset implements AssetInterface {
 
   /**
    * @throws \Drupal\Core\Asset\Exception\UnsupportedAsseticBehaviorException
--- core/lib/Drupal/Core/Asset/BaseAsset.php
+++ core/lib/Drupal/Core/Asset/BaseAsset.php
@@ -7,8 +7,6 @@
 
 namespace Drupal\Core\Asset;
 
-use Assetic\Filter\FilterCollection;
-use Assetic\Filter\FilterInterface;
 use Drupal\Core\Asset\AssetInterface;
 use Drupal\Core\Asset\Metadata\AssetMetadataBag;
 
@@ -24,18 +22,6 @@ use Drupal\Core\Asset\Metadata\AssetMetadataBag;
  */
 abstract class BaseAsset extends AsseticAdapterAsset implements AssetInterface, AssetOrderingInterface {
 
-  protected $filters;
-
-  protected $sourceRoot;
-
-  protected $sourcePath;
-
-  protected $targetPath;
-
-  protected $content;
-
-  protected $loaded;
-
   /**
    * @var AssetMetadataBag
    */
@@ -48,15 +34,12 @@ abstract class BaseAsset extends AsseticAdapterAsset implements AssetInterface, 
   protected $predecessors = array();
 
   public function __construct(AssetMetadataBag $metadata, $filters = array(), $sourceRoot = NULL, $sourcePath = NULL) {
-    $this->filters = new FilterCollection($filters);
-    $this->sourceRoot = $sourceRoot;
-    $this->sourcePath = $sourcePath;
-    $this->loaded = FALSE;
     $this->metadata = $metadata;
+    parent::__construct($filters, $sourceRoot, $sourcePath);
   }
 
   public function __clone() {
-    $this->filters = clone $this->filters;
+    parent::__clone();
     $this->metadata = clone $this->metadata;
   }
 
@@ -77,115 +60,8 @@ abstract class BaseAsset extends AsseticAdapterAsset implements AssetInterface, 
   /**
    * {@inheritdoc}
    */
-  public function ensureFilter(FilterInterface $filter) {
-    $this->filters->ensure($filter);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getFilters() {
-    return $this->filters->all();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function clearFilters() {
-    $this->filters->clear();
-  }
-
-  /**
-   * Encapsulates asset loading logic.
-   *
-   * @param string          $content          The asset content
-   * @param FilterInterface $additionalFilter An additional filter
-   */
-  protected function doLoad($content, FilterInterface $additionalFilter = NULL) {
-    $filter = clone $this->filters;
-    if ($additionalFilter) {
-      $filter->ensure($additionalFilter);
-    }
-
-    $asset = clone $this;
-    $asset->setContent($content);
-
-    $filter->filterLoad($asset);
-    $this->content = $asset->getContent();
-
-    $this->loaded = TRUE;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function dump(FilterInterface $additionalFilter = NULL) {
-    if (!$this->loaded) {
-      $this->load();
-    }
-
-    $filter = clone $this->filters;
-    if ($additionalFilter) {
-      $filter->ensure($additionalFilter);
-    }
-
-    $asset = clone $this;
-    $filter->filterDump($asset);
-
-    return $asset->getContent();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getContent() {
-    return $this->content;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setContent($content) {
-    $this->content = $content;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getSourceRoot() {
-    return $this->sourceRoot;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getSourcePath() {
-    return $this->sourcePath;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getTargetPath() {
-    return $this->targetPath;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setTargetPath($targetPath) {
-    $this->targetPath = $targetPath;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function isPreprocessable() {
     return (bool) $this->metadata->get('preprocess');
-  }
-
-  public function setDefaults(array $defaults) {
-    $this->metadataDefaults = $defaults;
   }
 
   /**
