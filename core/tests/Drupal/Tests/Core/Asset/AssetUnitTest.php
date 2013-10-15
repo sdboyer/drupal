5b7c20f1a23aab821ae7c483fc6f2fd672fed3c5
sdboyer <drupal@samboyer.org>
2013-10-14 22:56:42 -0400

    Add phpdoc to AssetUnitTest::createMockFileAsset().

--- core/tests/Drupal/Tests/Core/Asset/AssetUnitTest.php
+++ core/tests/Drupal/Tests/Core/Asset/AssetUnitTest.php
@@ -6,6 +6,7 @@
  */
 
 namespace Drupal\Tests\Core\Asset;
+use Drupal\Core\Asset\FileAsset;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -13,6 +14,17 @@ use Drupal\Tests\UnitTestCase;
  */
 abstract class AssetUnitTest extends UnitTestCase {
 
+  /**
+   * Creates a mock file asset.
+   *
+   * The mock will respond only to getAssetType() (with the provided type) and
+   * id(), with a randomly generated name.
+   *
+   * @param string $type
+   *   'css' or 'js'.
+   *
+   * @return FileAsset
+   */
   public function createMockFileAsset($type) {
     $asset = $this->getMock('Drupal\\Core\\Asset\\FileAsset', array(), array(), '', FALSE);
     $asset->expects($this->any())
